b1ebda2b43d9425dea78735d422fc7161dcb5a14
Sergey Nikishkin <sergey@gohopscotch.com>
2019-03-26 22:29:07 +0300

    init

---
+++ BitcoindApi/BitcoindApi/Controllers/BitcoinController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BitcoindApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BitcoinController : ControllerBase
+    {
+        public void SendBtc(string address, decimal amount)
+        {
+            
+        }
+    }
+}
