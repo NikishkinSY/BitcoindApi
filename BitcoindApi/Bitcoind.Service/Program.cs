b05fccf587c6c19a9696aec51f9076f1b034685c
Sergey Nikishkin <sergey@gohopscotch.com>
2019-03-28 18:08:07 +0300

    update Main method

--- BitcoindApi/Bitcoind.Service/Program.cs
+++ BitcoindApi/Bitcoind.Service/Program.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.WindowsServices;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 
 namespace Bitcoind.Service
 {
@@ -12,31 +7,9 @@ namespace Bitcoind.Service
     {
         public static void Main(string[] args)
         {
-            var isService = !(Debugger.IsAttached || args.Contains("--console"));
-            var pathToContentRoot = Directory.GetCurrentDirectory();
-            var webHostArgs = args.Where(arg => arg != "--console").ToArray();
-
-            if (isService)
-            {
-                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
-                pathToContentRoot = Path.GetDirectoryName(pathToExe);
-            }
-
-            var host = WebHost.CreateDefaultBuilder(webHostArgs)
-                .UseContentRoot(pathToContentRoot)
-                .UseStartup<Startup>()
-                .Build();
-
-            if (isService)
-            {
-                host.RunAsService();
-            }
-            else
-            {
-                host.Run();
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
-
+        
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
